413385ff0be0ebd485eb979c80320c46e4d12e4b
ahmedkawsar21 <49250362+ahmedkawsar21@users.noreply.github.com>
2019-04-11 02:39:19 -0400

    Add files via upload

---
+++ ArrayListPractice.java
@@ -0,0 +1,12 @@
+package structure;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+    public static void main(String[]args) {
+        ArrayList<Object> arrayList = new ArrayList<>();
+        arrayList.add("Kawsar");
+        arrayList.add("Abu");
+
+    }
+}
